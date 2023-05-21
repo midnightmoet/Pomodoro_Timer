196d83ad0f8322385b66ae8473b123954386deea
Lisa Miner <midnightmoet@gmail.com>
2023-05-21 18:22:06 -0400

    App styled and functional

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### Pomodoro Timer
 
-This is a simple pomodoro timer that I made to practice my JavaScript skills. It is a command line application that uses the `time` module to keep track of time. It also uses the `playsound` module to play a sound when the timer is up.
+This is a simple pomodoro timer that I made to practice my JavaScript skills. 
 
--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-const timerEl = document.getElementById('timer');
-const startEl = document.getElementById('start');
-const stopEl = document.getElementById('stop');
-const resetEl = document.getElementById('reset');
+const startEl = document.getElementById("start");
+const stopEl = document.getElementById("stop");
+const resetEl = document.getElementById("reset");
+const timerEl = document.getElementById("timer");
+
+let interval;
+let timeLeft = 1500;
+
+function updateTimer() {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+  let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+
+  timerEl.innerHTML = formattedTime;
+}
 
 function startTimer() {
-    console.log("start");
+  interval = setInterval(() => {
+    timeLeft--;
+    updateTimer();
+    if (timeLeft === 0) {
+      clearInterval(interval);
+      alert("Time's up!");
+      timeLeft = 1500;
+      updateTimer();
+    }
+  }, 1000);
 }
-
 function stopTimer() {
-    console.log("stop");
+  clearInterval(interval);
 }
-
 function resetTimer() {
-    console.log("reset");
+  clearInterval(interval);
+  timeLeft = 1500;
+  updateTimer();
 }
 
-startEl.addEventListener('click', startTimer);
-stopEl.addEventListener('click', stopTimer);
-resetEl.addEventListener('click', resetTimer);
 
-
+startEl.addEventListener("click", startTimer);
+stopEl.addEventListener("click", stopTimer);
+resetEl.addEventListener("click", resetTimer);
