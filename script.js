0d97a14a19f9ede0d99ac34a52b07fcaee6c3d42
Lisa Miner <midnightmoet@gmail.com>
2023-05-21 17:47:22 -0400

    Working on functionality, currently just console logging

---
+++ script.js
@@ -0,0 +1,22 @@
+const timerEl = document.getElementById('timer');
+const startEl = document.getElementById('start');
+const stopEl = document.getElementById('stop');
+const resetEl = document.getElementById('reset');
+
+function startTimer() {
+    console.log("start");
+}
+
+function stopTimer() {
+    console.log("stop");
+}
+
+function resetTimer() {
+    console.log("reset");
+}
+
+startEl.addEventListener('click', startTimer);
+stopEl.addEventListener('click', stopTimer);
+resetEl.addEventListener('click', resetTimer);
+
+
